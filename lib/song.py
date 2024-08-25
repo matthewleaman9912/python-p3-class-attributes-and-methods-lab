5a7ce3bdab1682d29afd6570526ff9e17c74480e
MatthewLeaman <leaman.30@osu.edu>
2024-08-24 21:52:03 -0400

    comp[lete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,56 @@
+from collections import Counter
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        print(self.genre_count)
+    @classmethod
+    def add_song_to_count(cls, increment = 1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre in cls.genres:
+            return cls.genres
+        else:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist in cls.artists:
+            return cls.artists
+        else:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        keys = []
+        values = []
+        
+        for listed in cls.genres:
+            if listed not in keys:
+                keys.append(listed)
+                values.append(1)
+        dicti = dict(zip(keys, values))
+        print(dicti)
+        
+
+
+               
+
+
+    
+
+
